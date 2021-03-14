d15ccf6ac91828dc88aa3135d46824a8b5306321
Riley <rsbeach97@gmail.com>
2021-03-14 09:24:12 -0700

    exported typdef and resolve

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql'
+  uri: '/graphql'
 });
-
 function App() {
   return (
     <ApolloProvider client={client}>
