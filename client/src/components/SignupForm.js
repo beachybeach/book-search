8a3b52dbeb08473072bdf2e38934841ec3b360c7
Riley <rsbeach97@gmail.com>
2021-03-20 08:39:28 -0700

    modifying client side pages/components/utils

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { CREATE_USER } from '../utils/mutations';
+import { userMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  const [createUser] = useMutation(CREATE_USER);
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
