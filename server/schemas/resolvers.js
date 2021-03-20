2cd3eba5c1db1bbd631406d022dc7fb35070d287
Riley <rsbeach97@gmail.com>
2021-03-20 08:07:26 -0700

    resolver query done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,11 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-
+      if (context.user) {
+        const userData = await (await User.findOne({ _id: context.user._id })).select("-__v -password");
+        return userData;
+      }
+      throw new AuthenticationError('Not Logged In!');
     }
   },
 
