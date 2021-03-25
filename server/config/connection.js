5b6bfde2f6ca39ebbe8b2cd04e0672f23ee14500
Riley <rsbeach97@gmail.com>
2021-03-24 21:16:44 -0700

    wesley sucks

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
-  useUnifiedTopology: false,
+  useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: true,
 });
